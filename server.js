07f66075339272d41d341757a59ceba0a1e4b4d3
Damien Asseya <damienasseya@Users-MacBook-Pro.local>
2017-06-11 23:28:57 -0400

    Improved the styles for the App

--- server.js
+++ server.js
@@ -22,4 +22,6 @@ app.set("view engine", "handlebars");
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-app.listen(PORT);
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
